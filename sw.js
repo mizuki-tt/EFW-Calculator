ebdcd98abab636bb035cff0aec4d32cc29d92fe2
mizuki-tt <mz34@hotmail.co.jp>
2026-08-24 22:51:20 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-const CACHE="efw-ig21-10-90-v3";
+const CACHE="efw-intergrowth-stable-input-v5";
 const ASSETS=["./","./index.html","./manifest.json","./sw.js"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+self.addEventListener("install",e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
 ));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim())
+));
+self.addEventListener("fetch",e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
