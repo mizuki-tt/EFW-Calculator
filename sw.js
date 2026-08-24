7d1553605e1e141d0f070c37bedd3755becde99f
mizuki-tt <mz34@hotmail.co.jp>
2026-08-24 22:19:41 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE="efw-calculator-final-v1";
+const CACHE_NAME="efw-calculator-fixed-v2";
 const ASSETS=["./","./index.html","./manifest.json","./sw.js"];
 self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+ event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 self.addEventListener("activate",event=>{
-  event.waitUntil(self.clients.claim());
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener("fetch",event=>{
-  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
 });
